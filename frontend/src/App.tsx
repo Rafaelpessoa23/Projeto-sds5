7d21215bf0440cea232d3fc87fc8b74130597c60
Rafaelpessoa23 <rafaelpessoa@id.uff.br>
2021-12-04 09:36:52 -0300

    Static Charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
+import React from 'react';
 import { DataTable } from 'Components/DataTable/DataTable';
 import { Footer } from 'Components/Footer/Footer';
 import { NavBar } from 'Components/NavBar';
-import React from 'react';
+import { BarChart } from 'Components/BarChart/BarChart';
+import { DonutChart } from 'Components/DonutChart/DonutChart';
 
 
 function App() {
@@ -9,7 +11,20 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className='text-primary'>Olá, Mundo!</h1>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas as vendas</h5>
+            <DonutChart />
+          </div>
+          <div className="py-3">
+            <h2 className="text-primary">Todas as Vendas</h2>
+          </div>
+        </div>
         <DataTable />
       </div>
       <Footer />
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'Assets/Css/styles.css';
+import 'Assets/css/styles.css';
 import App from './App';
 
 
